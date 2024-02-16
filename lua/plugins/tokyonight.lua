215d2bcae23c4c69991791d58d49c3226f771732
Remi DROUIN <rdrouin@graimatterlabs.ai>
2024-02-16 17:26:40 +0100

    feature(lua/plugins/tokyonight.lua): Add border between buffers

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -7,6 +7,7 @@ return {
       -- sidebars = { "qf", "vista_kind", "terminal", "packer" },
       on_colors = function(colors)
         colors.green = "#19A319"
+        colors.border = "#406ea7"
       end,
     },
   },

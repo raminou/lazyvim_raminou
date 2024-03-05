4dcad5e3288df8a54557c7786ccbb4e483a3ee34
Remi DROUIN <rdrouin@graimatterlabs.ai>
2024-03-05 11:58:19 +0100

    feature(lua/plugins/nvim-biscuits.lua): Add biscuit to hightlight end block

---
+++ lua/plugins/nvim-biscuits.lua
@@ -0,0 +1,13 @@
+return {
+  "code-biscuits/nvim-biscuits",
+  name = "nvim-biscuits",
+  version = "*",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    toggle_keybind = "<leader>cb",
+    show_on_start = false
+  },
+}
+
